813a9da3111bfb3bba6cdcebe8a5f145db426b35
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2023-06-11 22:20:37 +0800

    Removed Middle Man API class

--- pmssc/settings.py
+++ pmssc/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'home',
-    # 'product',
-    # 'blog',
+    'category'
+    'product',
+    'blog',
     # 'api',
     'authentication',
     'system_manager'
